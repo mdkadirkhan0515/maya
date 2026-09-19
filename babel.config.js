8cd1956afd69c7447997d0d709c6b02160048f95
KADIR_KHAN <mdkadirkhan0515@gmail.com>
2026-09-19 16:26:22 +0600

    Add module resolver plugin with aliases to Babel config

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,24 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./'],
+          extensions: ['.ts', '.tsx', '.js', '.json'],
+          alias: {
+            '@': './src',
+            '@components': './src/components',
+            '@screens': './src/screens',
+            '@services': './src/services',
+            '@constants': './src/constants',
+            '@types': './src/types',
+            '@navigation': './src/navigation'
+          }
+        }
+      ],
+      'react-native-reanimated/plugin',
+    ],
   };
 };
