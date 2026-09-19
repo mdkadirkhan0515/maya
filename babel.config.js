03dc88b2f77b920921740f215ef38b8cd3fcc7d4
KADIR_KHAN <mdkadirkhan0515@gmail.com>
2026-09-19 16:12:03 +0600

    Simplify babel.config.js by removing aliases
    Removed module resolver aliases from Babel configuration.

--- babel.config.js
+++ babel.config.js
@@ -1,20 +1,7 @@
-module.exports = function(api){
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: [
-      ['module-resolver', {
-        alias: {
-          '@': './src',
-          '@components': './src/components',
-          '@screens': './src/screens',
-          '@services': './src/services',
-          '@constants': './src/constants',
-          '@types': './src/types',
-          '@navigation': './src/navigation'
-        }
-      }],
-      'react-native-reanimated/plugin'
-    ]
-  }
-}
+    plugins: ['react-native-reanimated/plugin'],
+  };
+};
