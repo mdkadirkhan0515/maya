e2ca41d8d5b781284879346c7c53a2333fa53a4f
KADIR_KHAN <mdkadirkhan0515@gmail.com>
2026-09-19 14:38:43 +0600

    Refactor babel.config.js for cleaner formatting

--- babel.config.js
+++ babel.config.js
@@ -1,24 +1,20 @@
-module.exports = function (api) {
+module.exports = function(api){
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      [
-        'module-resolver',
-        {
-          root: ['./'],
-          alias: {
-            '@': './src',
-            '@components': './src/components',
-            '@screens': './src/screens',
-            '@services': './src/services',
-            '@constants': './src/constants',
-            '@types': './src/types',
-            '@navigation': './src/navigation',
-          },
-        },
-      ],
-      'react-native-reanimated/plugin',
-    ],
-  };
-};
+      ['module-resolver', {
+        alias: {
+          '@': './src',
+          '@components': './src/components',
+          '@screens': './src/screens',
+          '@services': './src/services',
+          '@constants': './src/constants',
+          '@types': './src/types',
+          '@navigation': './src/navigation'
+        }
+      }],
+      'react-native-reanimated/plugin'
+    ]
+  }
+}
